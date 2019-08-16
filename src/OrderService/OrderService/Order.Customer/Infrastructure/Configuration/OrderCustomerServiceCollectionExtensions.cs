bbfa41a5392add0c929e9883a1a005cb2045b719
klepeis <mattklepeis@gmail.com>
2019-08-16 10:00:21 -0400

    Add CustomerProfileClient to Orders service.

---
+++ src/OrderService/OrderService/Order.Customer/Infrastructure/Configuration/OrderCustomerServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OrderCustomerServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterOrderCustomer(this IServiceCollection services)
+        {
+            services.RegisterCustomerClient();
+
+            return services;
+        }
+    }
+}
